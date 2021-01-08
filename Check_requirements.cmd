df43fc5693470aa95a9987f0f50b1dc54e5667e4
deshwal88 <deshwalkanu88@gmail.com>
2021-01-08 23:59:15 +0530

    initial commit

--- Check_requirements.cmd
+++ Check_requirements.cmd
@@ -1,3 +1,5 @@
+curl "http://www.fit.vutbr.cz/~imikolov/rnnlm/simple-examples.tgz" -o "data.tgz"
+tar -cvzf "data.tgz"
 pip install numpy==1.19.4
 pip install tensorflow==2.3.1
 pip install grpcio==1.32.0
